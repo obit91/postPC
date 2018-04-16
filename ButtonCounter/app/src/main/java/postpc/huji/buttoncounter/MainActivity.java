56702f8bd6a9952113f83a83262777fdc278f903
oBit91 <ohad.beltzer@mail.huji.ac.il>
2018-04-16 11:38:42 +0300

    Uploading first exercise

---
+++ ButtonCounter/app/src/main/java/postpc/huji/buttoncounter/MainActivity.java
@@ -0,0 +1,43 @@
+package postpc.huji.buttoncounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+    TextView textView;
+    View button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.counterButton);
+        updateNumberOfTaps();
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                counter++;
+                updateNumberOfTaps();
+            }
+        });
+    }
+
+    private void updateNumberOfTaps() {
+        if (counter % 7 == 0 && counter != 0)
+            textView.setText("B O O M");
+        else
+            textView.setText(String.valueOf(counter));
+
+    }
+}
